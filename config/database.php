19f6dac11f6868b15789fa41275d461ce450a4b2
samado11 <omarabdelsamad7@gmail.com>
2022-01-14 04:09:55 +0200

    env

--- config/database.php
+++ config/database.php
@@ -48,7 +48,6 @@ return [
             'driver' => 'mysql',
             // 'url' => env('DATABASE_URL'),
             'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
             'database' => ltrim($DATABASE_URL["path"],"/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
